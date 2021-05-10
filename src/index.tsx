b16189d5b2b0a864e03e513c0017006a75f49649
Matteo Lobello <matteo.lobello@protonmail.com>
2021-05-10 13:14:19 +0200

    add service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { App } from "./App"
+// @ts-ignore
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration"
 import "./index.css"
 
 ReactDOM.render(
@@ -9,3 +11,14 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById("root")
 )
+
+serviceWorkerRegistration.register({
+    onUpdate: (registration: any) => {
+        console.log("New version available!")
+
+        if (registration && registration.waiting) {
+            registration.waiting.postMessage({ type: "SKIP_WAITING" })
+        }
+        window.location.reload()
+    },
+})
